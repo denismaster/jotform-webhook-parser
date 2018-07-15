79545bff50671c570da0027f46c2bf24bf7b1eb0
denismaster <denismaster@outlook.com>
2018-07-15 12:35:46 +0300

    Add test for handling "qN_" part of properties
    In each submission that comes to webhook, there is a "qN_" prefix, where N is the number of question in JotForm.
    We need to remove this prefix so then we could serialize result JSON to our Type

--- tests/JotFormWebhookParser.Tests/JotFormParserTests.cs
+++ tests/JotFormWebhookParser.Tests/JotFormParserTests.cs
@@ -18,5 +18,25 @@ namespace JotFormWebhookParser.Tests
 
             Assert.Null(result);
         }
+
+        [Fact]
+        public void ShouldRemoveFirstPartOfPropertyName()
+        {
+            var submission = new JObject
+            {
+                { "q1_test", "foo" }
+            };
+
+            var parser = new JotFormParser();
+
+            var result = parser.Parse(submission);
+
+            Assert.NotNull(result);
+
+            Assert.True(result.Properties().Count() == 1);
+            Assert.Contains(result.Properties(), c => c.Name == "test");
+
+            Assert.Equal("foo", result["test"]);
+        }
     }
 }
